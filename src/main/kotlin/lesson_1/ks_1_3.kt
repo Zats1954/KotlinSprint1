d8f9026e65c763a131df44d12fcbec4b2329728a
Zats1954 <alex.zacepin@gmail.com>
2025-11-30 21:43:00 +0100

    KS_1_2 polet Gagarina

---
+++ src/main/kotlin/lesson_1/ks_1_3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+fun main() {
+    val firstCosmosManYear = 1961
+    var hour = "09"
+    var minute = "17"
+
+    println(" год полета Юрия Гагарина в космос $firstCosmosManYear")
+    println(" час взлета Юрия Гагарина в космос $hour")
+    println(" минута взлета Юрия Гагарина в космос $minute\n")
+
+    hour = "10"
+    minute = "55"
+    println(" время посадки Юрия Гагарина из космоса $hour:$minute")
+}
